f398d5379ee5f4fb6e00f5f4345c9929b8bc0c12
RBS3 <rana.ben.salem.10@gmail.com>
2025-04-13 20:24:10 +0100

    updated1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import LoginSignup from './Components/LoginSignup/LoginSignup';
-import Dashboard from "./Components/Dashboard/Dashboard";
-import Jobapp from "./Components/job_application/Jobapp";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginSignup />} />
-        <Route path="/Dashboard" element={<Dashboard />} />
         
       </Routes>
     </Router>
